849bb79981b00744f30deadc9dd46f4351a723b3
Michael Schats <michael_schats@hotmail.com>
2021-01-15 09:36:07 +0800

    Full set-up, plus Rspec & simplecov functioning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,18 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+                                                                 SimpleCov::Formatter::Console
+                                                               ])
+
+SimpleCov.start
+require_relative '../lib/gilded_rose.rb'
+require_relative '../lib/item.rb'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
